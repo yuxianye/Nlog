4dd77f4e00bc379991558fb786064da77d065e0d
yuxianye <yuxianye@163.com>
2020-07-16 17:32:05 +0800

    增加AspNetCode NLogDemo

---
+++ AspNetCoreNLog/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AspNetCoreNLog.Models;
+
+namespace AspNetCoreNLog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _logger.LogInformation("日志测试");
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("日志测试");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            _logger.LogInformation("日志测试");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            _logger.LogInformation("日志测试");
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

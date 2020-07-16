3ce4520aa8de37db942dc027cf82cec3cfc1bf41
yuxianye <yuxianye@163.com>
2020-07-16 16:49:12 +0800

    增加直接调用的demo

---
+++ LogHelper.Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LogHelper.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            LogHelper.Logger.Debug("测试");
+            LogHelper.Logger.Error("测试");
+        }
+    }
+}
